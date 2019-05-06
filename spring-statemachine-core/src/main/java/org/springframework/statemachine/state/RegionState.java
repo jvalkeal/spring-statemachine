1f3c1c3022acba1cdc589ca36bdeb32b64e77e91
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-05-06 09:13:35 +0100

    stash

--- spring-statemachine-core/src/main/java/org/springframework/statemachine/state/RegionState.java
+++ spring-statemachine-core/src/main/java/org/springframework/statemachine/state/RegionState.java
@@ -17,11 +17,11 @@ package org.springframework.statemachine.state;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.function.Function;
 
 import org.springframework.messaging.Message;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachineEventResult;
-import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.region.Region;
 import org.springframework.statemachine.region.RegionExecutionPolicy;
 import org.springframework.statemachine.support.StateMachineUtils;
@@ -85,7 +85,8 @@ public class RegionState<S, E> extends AbstractState<S, E> {
 	 * @param pseudoState the pseudo state
 	 */
 	public RegionState(S id, Collection<Region<S, E>> regions, Collection<E> deferred,
-			Collection<? extends Action<S, E>> entryActions, Collection<? extends Action<S, E>> exitActions, PseudoState<S, E> pseudoState) {
+			Collection<Function<StateContext<S, E>, Mono<Void>>> entryActions,
+			Collection<Function<StateContext<S, E>, Mono<Void>>> exitActions, PseudoState<S, E> pseudoState) {
 		super(id, deferred, entryActions, exitActions, pseudoState, regions);
 	}
 
@@ -99,7 +100,8 @@ public class RegionState<S, E> extends AbstractState<S, E> {
 	 * @param exitActions the exit actions
 	 */
 	public RegionState(S id, Collection<Region<S, E>> regions, Collection<E> deferred,
-			Collection<? extends Action<S, E>> entryActions, Collection<? extends Action<S, E>> exitActions) {
+			Collection<Function<StateContext<S, E>, Mono<Void>>> entryActions,
+			Collection<Function<StateContext<S, E>, Mono<Void>>> exitActions) {
 		super(id, deferred, entryActions, exitActions, null, regions);
 	}
 
@@ -141,10 +143,8 @@ public class RegionState<S, E> extends AbstractState<S, E> {
 			return Flux.fromIterable(getRegions())
 				.flatMap(r -> r.stopReactively())
 				.then(Flux.fromIterable(getExitActions())
-					.doOnNext(ea -> {
-						executeAction(ea, context);
-					})
-				.then());
+					.flatMap(a -> a.apply(context))
+					.then());
 		}));
 	}
 
@@ -177,9 +177,7 @@ public class RegionState<S, E> extends AbstractState<S, E> {
 	public Mono<Void> entry(StateContext<S, E> context) {
 		return super.entry(context)
 			.and(Flux.fromIterable(getEntryActions())
-			.doOnNext(ea -> {
-				executeAction(ea, context);
-			})
+					.flatMap(a -> a.apply(context))
 			.then(startOrEntry(context)));
 	}
 
